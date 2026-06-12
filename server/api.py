195075a5a3121e5e08681da75103aefc5b4b122d
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-06-12 16:10:21 -0400

    Made API Return a 'Response' Instead of Str

--- server/api.py
+++ server/api.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, Response
 
 app = Flask(__name__)
 
 @app.route("/")
-def homepage() -> str:
-    return "Hello World"
+def homepage() -> Response:
+    return jsonify({
+        "success": True
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
