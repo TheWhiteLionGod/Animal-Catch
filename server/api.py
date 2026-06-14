1d1b5ae35f8b3b5f63984fb9a4edaae019dfa9ae
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-06-14 15:34:16 -0400

    Added Get Stats Route

--- server/api.py
+++ server/api.py
@@ -8,5 +8,14 @@ def homepage() -> Response:
         "success": True
     })
 
+@app.route("/getstats/<animalName>")
+def getStats(animalName: str) -> Response:
+    return jsonify({
+        "name": animalName,
+        "hp": 0,
+        "atk": 0,
+        "def": 0
+    })
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
