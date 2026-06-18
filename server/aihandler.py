9a59cfd189bcff5813eeb21ed0c1bf5925abfb4d
TheWhiteLionGod <aaryan12jul@gmail.com>
2026-06-18 15:29:13 -0400

    Created Animal Classification Model

--- server/aihandler.py
+++ server/aihandler.py
@@ -1,4 +1,14 @@
+from transformers import pipeline, ImageClassificationPipeline
+from PIL import Image
+import time
 import os
+
+def createClassifier() -> ImageClassificationPipeline:
+    return pipeline("image-classification", model="timm/vit_large_patch14_clip_336.laion2b_ft_augreg_inat21")
+
+def predictAnimal(classifier: ImageClassificationPipeline, image: Image) -> dict[str, str | float]:
+    predictions: list[dict] = classifier(image)
+    return {pred["label"]: pred['score'] for pred in predictions}
 
 def grabPrompt(filename: str) -> str:
     if not os.path.exists(f"prompts/{filename}.txt"):
@@ -8,5 +18,21 @@ def grabPrompt(filename: str) -> str:
         return f.read()
 
 if __name__ == '__main__':
+    # TODO: Delete Testing Code & deer.jpeg
+    start: float = time.perf_counter()
+    classifier = createClassifier()
+    
+    end: float = time.perf_counter()
+    print(f"Time to Load Model: {end - start:.2f}")
+    
+    image: Image = Image.open("deer.jpeg").convert("RGB")
+
+    start: float = time.perf_counter()
+    predictions = predictAnimal(classifier, image)
+    end: float = time.perf_counter()
+
+    print(predictions)
+    print(f"Time to Make Prediction: {end - start:.2f}")
+
     prompt: str = grabPrompt("test")
     print(prompt)
